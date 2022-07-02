5be6add242947de53c2f5f20660e07f7bd99116f
Moslem Yazdanpanah <Moslem.Yazdanpanah@gmail.com>
2022-07-02 23:58:17 +0430

    download_datasets

--- download_datasets.py
+++ download_datasets.py
@@ -16,5 +16,8 @@ def download(root: str, archive_name: str, url_link: str):
     download_url(url_link, root, archive_name)
    
 for i in range(1, len(datasets.__all__)):
-    for file in datasets.__dict__[datasets.__all__[i]].download_list:
-        download(os.path.join("~/scratch/TLlib_Dataset", datasets.__all__[i].lower()), archive_name = file[1], url_link = file[2])
+    try:
+        for file in datasets.__dict__[datasets.__all__[i]].download_list:
+            download(os.path.join("~/scratch/TLlib_Dataset", datasets.__all__[i].lower()), archive_name = file[1], url_link = file[2])
+    except Exception as e:
+        print(e)
