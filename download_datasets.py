f5eff4b471ad515a9e270e604a6043347b1690a2
Moslem Yazdanpanah <Moslem.Yazdanpanah@gmail.com>
2022-07-02 23:54:40 +0430

    download_datasets

--- download_datasets.py
+++ download_datasets.py
@@ -13,14 +13,8 @@ def download(root: str, archive_name: str, url_link: str):
         url_link: (str) The url link to download data.
     """
     
-    try:
-        download_url(url_link, download_root=root, filename=archive_name)
-    except Exception:
-        print("Fail to download {} from url link {}".format(archive_name, url_link))
-        print('Please check you internet connection.'
-                "Simply trying again may be fine.")
-        exit(0)
-            
+    download_url(url_link, root, archive_name)
+   
 for i in range(1, len(datasets.__all__)):
     for file in datasets.__dict__[datasets.__all__[i]].download_list:
         download(os.path.join("~/scratch/TLlib_Dataset", datasets.__all__[i].lower()), archive_name = file[1], url_link = file[2])
